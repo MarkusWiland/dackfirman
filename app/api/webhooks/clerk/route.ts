329ded774d90d7637bed49ad609793ac4ab94a61
Markus Wiland <markus.wiland@outlook.com>
2024-10-31 13:12:07 +0100

    asdasd

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,64 +1,69 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
 
-import { createUser } from '@/lib/users'
+import { createUser } from "@/lib/users";
+import db from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
 
   // Get the headers
-  const headerPayload = await headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
+    return new Response("Error occured -- no svix headers", {
       status: 400,
-    })
+    });
   }
 
   // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET)
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent
+  let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
-    }) as WebhookEvent
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error occured', {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
       status: 400,
-    })
+    });
   }
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data
-  const eventType = evt.type
-  if(eventType === "user.created") {
-    const {id, email_addresses, first_name, image_url} = evt.data
+  const { id } = evt.data;
+  const eventType = evt.type;
+  if (eventType === "user.created") {
+    const { id, email_addresses, first_name, image_url } = evt.data;
 
-    if(!id || !email_addresses) {
+    if (!id || !email_addresses) {
       return new Response("Error", {
-        status: 400
-      })
+        status: 400,
+      });
     }
 
     const user = {
@@ -66,11 +71,14 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       name: first_name ?? "",
       image: image_url,
+    };
+    let dbUser = db.select().from(users).where(eq(users.id, id));
+    if (!dbUser) {
+      await createUser(user);
     }
-    await createUser(user)
   }
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
-  return new Response('', { status: 200 })
+  return new Response("", { status: 200 });
 }
