b23e61362c3db76ab647128d52c7d0be4f8792cc
Markus Wiland <markus.wiland@outlook.com>
2024-10-31 12:21:11 +0100

    qwe

--- lib/users.ts
+++ lib/users.ts
@@ -1,6 +1,9 @@
+import db from "@/db"
+import { users } from "@/db/schema"
+
 export async function createUser(user: any) {
   try {
-
+      await db.insert(users).values(user)
   } catch(err) {
     return {err}
   }
